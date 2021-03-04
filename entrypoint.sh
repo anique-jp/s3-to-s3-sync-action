84a4d4451f8639ca252dc58a10a26ab9cd531678
lobo1981 <info@sashimi.dev>
2021-03-04 16:10:50 +0900

    remove environment variable check of AWS_S3_BUCKET

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [ -z "$AWS_S3_BUCKET" ]; then
-  echo "AWS_S3_BUCKET is not set. Quitting."
-  exit 1
-fi
-
 if [ -z "$AWS_ACCESS_KEY_ID" ]; then
   echo "AWS_ACCESS_KEY_ID is not set. Quitting."
   exit 1
